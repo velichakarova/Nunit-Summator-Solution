41cc9b5678c87d8daa271ecd1005ad445b89e4a2
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2022-05-17 13:14:12 +0100

    Commit Project

---
+++ Summator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+           if(result == 2)
+            {
+                Console.WriteLine("Test Pass!");
+            }else
+            {
+                Console.WriteLine("Test Fail!");
+            }
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+
+        public static int Sum(int[] arr)
+        {
+            checked
+            {
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                return sum;
+
+            }
+        }
+        public static int Average(int[] arr)
+        {
+            checked
+            {
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                return sum/arr.Length;
+
+            }
+        }
+    }
+}
