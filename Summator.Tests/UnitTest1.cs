ae81052e19a998ee5f86413f7ac2a0a2652a4f2f
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2022-05-18 19:02:52 +0100

    Nunit Tests

---
+++ Summator.Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+namespace Summator.Tests
+{
+    public class Tests
+    {
+        [Test]
+      public void Test_SumTwoPossitiveNumbers()
+        {
+            int result = Summator.Sum(new int[] { 5, 7 });
+
+            Assert.That(result == 12);
+        }
+        [Test]
+        public void Test_Sum_MultiplePossitiveNumbers()
+        {
+            int result = Summator.Sum(new int[] { 5, 7,8,9,10 });
+
+            Assert.That(result == 39);
+        }
+        [Test]
+        public void Test_SumOnePossitiveNumbers()
+        {
+            int result = Summator.Sum(new int[] { 5 });
+
+            Assert.That(result == 5);
+        }
+        [Test]
+        public void Test_SumTwoNegativePossitiveNumbers()
+        {
+            int expect = Summator.Sum(new int[] { -5,-7 });
+            int actual = -12;
+
+             Assert.That(expect == actual);
+        }
+        [Test]
+        public void Test_EmpetyArr()
+        {
+            int expect = Summator.Sum(new int[] {  });
+            int actual = 0;
+
+            Assert.That(expect == actual);
+        }
+        [Test]
+        public void Test_BigValues()
+        {
+            int expected = Summator.Sum(new int[] { 2000000000, 2000000000 , 2000000000, });
+
+            Assert.That(expected, Is.EqualTo(6000000000)); 
+        }
+        [Test]
+        public void Test_Average()
+        {
+            int expected = Summator.Average(new int[] { 3,3,3 });
+            int actual = 3;
+            Assert.That(expected==actual);
+        }
+    }
+}
